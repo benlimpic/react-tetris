5b25cf3500fc148408e3e885006d17c9a8147862
init.defaultBranch <benlimpic@gmail.com>
2022-10-12 13:03:19 -0700

    custom hooks and game over conditional rendering

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { createStage } from '../gameHelpers';
+//styled Components
 import { StyledTetrisWrapper, StyledTetris} from './styles/StyledTetris';
+
+//Custom Hooks
+import { usePlayer } from './hooks/usePlayer';
+import { useStage } from './hooks/useStage';
 
 //components
 import Stage from './Stage';
@@ -9,16 +13,28 @@ import Display from './Display';
 import StartButton from './StartButton';
 
 const Tetris = () => {
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  const [player] = usePlayer();
+  const [stage, setStage] = useState(player);
+
+
+  console.log('re-render');
   return(
     <StyledTetrisWrapper>
       <StyledTetris>
-      <Stage stage={createStage()} />
+      <Stage stage={stage} />
       <aside>
+        {gameOver ? (
+          <Display gameOver={gameOver} text="Game Over" />
+        ) : (
         <div>
           <Display text="Score" />
           <Display text="Rows" />
           <Display text="Level" />
         </div>
+        )}
         <StartButton />
       </aside>
       </StyledTetris>
