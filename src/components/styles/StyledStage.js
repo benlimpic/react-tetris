67a45af4d1ad30b01177c5a110bba68ed3a5f5ea
init.defaultBranch <benlimpic@gmail.com>
2022-10-12 01:29:01 -0700

    all styles completed and margin issues with side bar fixed

--- src/components/styles/StyledStage.js
+++ src/components/styles/StyledStage.js
@@ -6,11 +6,11 @@ export const StyledStage = styled.div`
     ${props => props.height},
     calc(25vw / ${props => props.width})
   );
-  
+
   grid-template-columns: repeat(${props => props.width}, 1fr);
   grid-gap: 1px;
-  border: 2px wsolid #333;
+  border: 2px solid #333;
   width: 100%;
   max-width: 25vw;
   background: #111;
-`
+`;
--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -6,9 +6,9 @@ export const StyledTetrisWrapper = styled.div`
     width: 100vw;
     height: 100vh;
     background: url(${bgImage}) #000;
-    background-sixe: cover;
+    background-size: cover;
     overflow: hidden;
-  `
+  `;
 
 export const StyledTetris = styled.div`
     display: flex;
@@ -23,4 +23,4 @@ export const StyledTetris = styled.div`
       display: block;
       padding: 0 20px;
     }
-  `
+  `;
