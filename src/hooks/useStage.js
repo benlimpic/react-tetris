51756b08c1f2f40002c6fc0635f4f8e944bc7a57
init.defaultBranch <benlimpic@gmail.com>
2022-10-12 16:32:53 -0700

    created collision detection, can move player left right and down, merge player with stage and initiate game over if y < 1

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
 
+import { useState, useEffect } from 'react';
 import { createStage } from '../gameHelpers';
 
 export const useStage = (player, resetPlayer) => {
@@ -10,9 +10,9 @@ export const useStage = (player, resetPlayer) => {
       // First flush the stage
       const newStage = prevStage.map(row =>
         row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
-        );
+      );
 
-      // Then draw tetromino
+      // Then draw the tetromino
       player.tetromino.forEach((row, y) => {
         row.forEach((value, x) => {
           if (value !== 0) {
@@ -23,12 +23,16 @@ export const useStage = (player, resetPlayer) => {
           }
         });
       });
+      // Then check if we collided
+      if (player.collided) {
+        resetPlayer();
+      }
 
       return newStage;
     };
 
     setStage(prev => updateStage(prev));
-  }, [player]);
+  }, [player, resetPlayer]);
 
   return [stage, setStage];
-}
+};
