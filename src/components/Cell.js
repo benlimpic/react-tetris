5c9da4638614732f0a511f81d851c3980171e0f6
init.defaultBranch <benlimpic@gmail.com>
2022-10-12 00:21:24 -0700

    Basic components and styling for Tetris game. Created pieces and assigned color properties, also introduced rational for selecting colors and styling the main page.

---
+++ src/components/Cell.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyledCell } from './styles/StyledCell'
+import { TETROMINOS } from '../tetrominos';
+
+const Cell = ({ type }) => (
+  <StyledCell type={'L'} color={TETROMINOS['L'].color}>cell</StyledCell>
+)
+
+export default Cell;
---
+++ src/components/StartButton.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const StartButton = ({ callback }) => (
+  <div>Start Game</div>
+)
+export default StartButton;
