c50d10aa5b4ec735948e99a74908507acba18c18
init.defaultBranch <benlimpic@gmail.com>
2022-10-12 17:30:34 -0700

    integrating automatic drop and instituted a new proticol for stopping the drop interval while the down arrow is dropping the tetrominos and starting it up agaoin on Key up

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -6,6 +6,7 @@ import { createStage, checkCollision } from '../gameHelpers';
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
 // Custom Hooks
+import { useInterval } from '../hooks/useInterval'
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
 
@@ -33,6 +34,7 @@ const Tetris = () => {
     console.log("test")
     // Reset everything
     setStage(createStage());
+    setDropTime(1000);
     resetPlayer();
     setGameOver(false);
   }
@@ -51,7 +53,16 @@ const Tetris = () => {
     }
   }
 
+  const keyUp = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 40) {
+        setDropTime(1000);
+      }
+    }
+  }
+
   const dropPlayer = () => {
+    setDropTime(null);
     drop();
   }
 
@@ -69,8 +80,12 @@ const Tetris = () => {
     }
   }
 
+  useInterval(() => {
+    drop();
+  }, dropTime)
+
   return (
-    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)} onKeyUp={keyUp}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
